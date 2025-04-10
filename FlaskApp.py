10aa5b1cfd898db63a0efafef58af4fe038055a9
ian-altekruse <ialtekruse@gmail.com>
2025-04-10 13:13:19 -0500

    Port Update

--- FlaskApp.py
+++ FlaskApp.py
@@ -29,4 +29,4 @@ def viewdb():
     return render_template_string(html, columns=columns, rows=rows)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
